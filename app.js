0391e8f1b8d0bf4ba345ca803718e42ece247680
Tyler Malone <tylermalone@me.com>
2023-02-21 12:14:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+
+app.listen(4000, () => {
+  console.log(`[server]: listening on 4000`);
+});
+
+app.get(`/home`, (request, response) => {
+  response.send("<h1> Hello, example!</h1>");
+});
+
+app.get(`/name`, (request, response) => {
+  response.send(`My name is Alex`);
+});
+
+app.get(`/friend`, (request, response) => {
+  response.send(`My friend is Tyler`);
+});
+
+app.get(`/:page`, (request, response) => {
+  console.log(request.params);
+  let page = request.params.page;
+  response.send(`you are on the ${page} page`);
+});
